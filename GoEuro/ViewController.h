4199af4357286474bbdd6e912b8a7fbf1382d51b
Zoltan Csipler <csipi17@gmail.com>
2016-06-16 18:42:16 +0200

    Initial Commit

---
+++ GoEuro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoEuro
+//
+//  Created by Zoltan Csipler on 16/06/16.
+//  Copyright © 2016 Zoltan Csipler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GoEuro/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoEuro
+//
+//  Created by Zoltan Csipler on 16/06/16.
+//  Copyright © 2016 Zoltan Csipler. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
